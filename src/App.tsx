e8fa65b5354bbfd969d66f9ecefbae64e07eb8d4
Ken saadi <ken.saadi@yahoo.com>
2021-09-18 20:27:16 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import tw, { styled } from 'twin.macro';
+
+const StyledApp = styled.div`
+	${tw`flex justify-center items-center h-screen`}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<StyledApp>
+			<header className='App-header'>
+				<p>Boilerplate React + Tailwind Css Styled-component twin.macro</p>
+				<a
+					className='text-gray-800 text-2xl'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					App.tsx
+				</a>
+			</header>
+		</StyledApp>
+	);
 }
 
 export default App;
